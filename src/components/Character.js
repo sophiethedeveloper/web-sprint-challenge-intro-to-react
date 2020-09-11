b8c0cba9db20d6858d23e505ba90467a3149a4aa
sofia Luna <Sofia21luna@gmail.com>
2020-09-11 09:41:51 -0600

    reconstructed code and formatted it correctly

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,36 +1,36 @@
 // Write your Character component here
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const CharacterCard = styled.div`
-    background-color: #99f3eb;
-    color: #000000;
-    width: 200px;
-    max-height: 300px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
-    margin-bottom: 20px;
+  background-color: #99f3eb;
+  color: #000000;
+  width: 200px;
+  max-height: 400px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 20px;
 `;
 
 const CharacterImg = styled.img`
-    width: 200px;
-    height: 200px;
-    object-fit: scale;
-    flex-shrink: 2;
+  width: 200px;
+  height: 200px;
+  object-fit: scale;
+  flex-shrink: 2;
 `;
 
+const Character = (props) => {
+  const { name, image, species } = props;
 
-
-const Character = props => {
-    const {name, image} = props
-    return (
-        <CharacterCard>
-            <h2>{name}</h2>
-            <CharacterImg alt="random character" src={image}/>
-        </CharacterCard>
-    )
-}
+  return (
+    <CharacterCard>
+      <h2>{name}</h2>
+      <CharacterImg alt="random character" src={image} />
+      <h3>{species}</h3>
+    </CharacterCard>
+  );
+};
 
 export default Character;
